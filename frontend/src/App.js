ed4a277e1f326706e950f6c4f92a48a4409bc84f
Tyler Friesen <tfriesen2@my.gcu.edu>
2023-07-15 12:41:37 -0600

    made grid squares to be controlled by React state instead of by a custom attribute of the button. Also fixed the hover properties of a button since setting button color from the Grid component broke the CSS.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,51 @@ import {useState} from 'react';
 const Grid = () => {
   const numRows = 100;
   const numColumns = 100;
-  const grid = useState([100][100]);
+  const [grid, setgrid] = useState(Array.from({ length: 100 }, () => new Array(100).fill(false)));
 
   const clickHandler = (row, col, buttonId) => {
     console.log("You clicked me!", row, col);
     let button = document.getElementById(buttonId);
     console.log(button.id)
-    if (!button.clickedstate) {
-      button.style.backgroundColor = "#FFFF00";
-      button.clickedstate = !button.clickedstate
-    } else {
+    const newState = grid.map((row, rowIndex) => {
+      return grid[rowIndex].map((square, index) => {
+        if ('button-' + rowIndex + '-' + index === buttonId) {
+          console.log(!grid[rowIndex][index])
+          console.log(square)
+          return !grid[rowIndex][index];
+        } else {
+          return square
+        }
+      })
+    })
+    setgrid(newState)
+  }
+
+  const mouseHovering = (row, col, buttonId) => {
+    let button = document.getElementById(buttonId);
+    if (grid[row][col] !== true) {
+      button.style.backgroundColor = "#ce1313";
+    }
+  }
+
+  const mouseLeaving = (row, col, buttonId) => {
+    let button = document.getElementById(buttonId);
+    if (grid[row][col] !== true) {
       button.style.backgroundColor = "#767575";
-      button.clickedstate = !button.clickedstate
+    }
+  }
+  
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      let button = document.getElementById('button-' + i + '-' + j);
+      if (grid[i][j] === true) {
+        button.style.backgroundColor = "#FFFF00";
+      } else if (grid[i][j] === false) {
+        if (button) {
+          button.style.backgroundColor = "#767575";
+          
+        }
+      }
     }
   }
 
@@ -25,7 +58,7 @@ const Grid = () => {
       for (let col = 0; col < numColumns; col++) {
         const buttonId = 'button-' + row + '-' + col;
         buttons.push(
-          <button key={buttonId} id={buttonId} clickedstate="false" className='grid-button' onClick={() => clickHandler(row, col, buttonId)}>
+          <button key={buttonId} id={buttonId} clickedstate="false" className='grid-button' onClick={() => clickHandler(row, col, buttonId)} onMouseEnter={() => mouseHovering(row, col, buttonId)} onMouseLeave={() => mouseLeaving(row, col, buttonId)}>
             
           </button>
         )
