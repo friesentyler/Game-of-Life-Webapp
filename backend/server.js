d20d48e3d113ae022f7c46d864de997fd0c84956
Tyler Friesen <tfriesen2@my.gcu.edu>
2023-07-19 10:02:02 -0600

    increased font size for buttons

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
   
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
+
+app.get('/boardstate', (req, res) => {
+    res.send('Hello World!')
+  })
+
   
 app.listen(PORT, (error) =>{
     if(!error)
