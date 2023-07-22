f1e59d7ac7fe32cfb9cd37f83cfe083441698245
Tyler Friesen <tfriesen2@my.gcu.edu>
2023-07-22 14:11:50 -0600

    worked on game logic, worked on /update endpoint, added index field to Schema

--- backend/Model.js
+++ backend/Model.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ModelSchema = new Schema({
+    index: {
+        type: Number,
+        required: true
+    },
     row: {
         type: Number,
         required: true
--- backend/server.js
+++ backend/server.js
@@ -23,17 +23,92 @@ db.once("open", () => {
     console.log("Connected successfully");
 });
 
+const liveNeighbors = (index, board) => {
+    let count = 0;
+    if (index === 0) {
+        count = board[index+1].alive == true ? count + 1 : count;
+        count = board[index+100].alive == true ? count + 1 : count;
+        count = board[index+101].alive == true ? count + 1 : count;
+    } else if (index === 9900) {
+        count = board[index-100].alive == true ? count + 1 : count;
+        count = board[index-99].alive == true ? count + 1 : count;
+        count = board[index+1].alive == true ? count + 1 : count;
+    } else if (index === 99) {
+        count = board[index-1].alive == true ? count + 1 : count;
+        count = board[index+99].alive == true ? count + 1 : count;
+        count = board[index+100].alive == true ? count + 1 : count;
+    } else if (index === 9999) {
+        count = board[index-1].alive == true ? count + 1 : count;
+        count = board[index-100].alive == true ? count + 1 : count;
+        count = board[index-101].alive == true ? count + 1 : count;
+    } else if (index % 100 === 0) {
+        count = board[index-100].alive == true ? count + 1 : count;
+        count = board[index-99].alive == true ? count + 1 : count;
+        count = board[index+1].alive == true ? count + 1 : count;
+        count = board[index+100].alive == true ? count + 1 : count;
+        count = board[index+101].alive == true ? count + 1 : count;
+    } else if (index % 100 === 99) {
+        count = board[index-100].alive == true ? count + 1 : count;
+        count = board[index-101].alive == true ? count + 1 : count;
+        count = board[index-1].alive == true ? count + 1 : count;
+        count = board[index+99].alive == true ? count + 1 : count;
+        count = board[index+100].alive == true ? count + 1 : count;
+    } else if (index <= 99) {
+        count = board[index-1].alive == true ? count + 1 : count;
+        count = board[index+1].alive == true ? count + 1 : count;
+        count = board[index+99].alive == true ? count + 1 : count;
+        count = board[index+100].alive == true ? count + 1 : count;
+        count = board[index+101].alive == true ? count + 1 : count;
+    } else if (index >= 9900) {
+        count = board[index-1].alive == true ? count + 1 : count;
+        count = board[index+1].alive == true ? count + 1 : count;
+        count = board[index-99].alive == true ? count + 1 : count;
+        count = board[index-100].alive == true ? count + 1 : count;
+        count = board[index-101].alive == true ? count + 1 : count;
+    } else {
+        count = board[index-1].alive == true ? count + 1 : count;
+        count = board[index+1].alive == true ? count + 1 : count;
+        count = board[index-99].alive == true ? count + 1 : count;
+        count = board[index-100].alive == true ? count + 1 : count;
+        count = board[index-101].alive == true ? count + 1 : count;
+        count = board[index+99].alive == true ? count + 1 : count;
+        count = board[index+100].alive == true ? count + 1 : count;
+        count = board[index+101].alive == true ? count + 1 : count;
+    }
+    return count;
+}
+
+const updateBoardState = (board) => {
+    let liveness = [];
+    for (let i = 0; i < board.length; i++) {
+        liveness.push(liveNeighbors(i, board));
+    }
+    for (let j = 0; j < liveness.length; j++) {
+        if (liveness[j] >= 2 && liveness[j] <= 3 && board[j].alive == true) {
+            board[j].alive = true;
+        } else if (liveness[j] == 3) {
+            board[j].alive = true;
+        } else {
+            board[j].alive = false;
+        }
+    }
+    return board;
+}
+
 app.post('/initialize', async (req, res) => {
     let states = [];
+    let count = 0;
     for (let i = 0; i < 100; i++) {
         for (let j = 0; j < 100; j++) {
             const model = new Model({
+                index: count,
                 row: i,
                 column: j,
                 alive: false,
             });
             model.save();
             states = states.concat(model);
+            count++;
         }
     }
     res.json(states);
@@ -45,13 +120,20 @@ app.put('/modify/:row/:column', async (req, res) => {
 })
 
 app.get('/boardstate', async (req, res) => {
-    const boardstate = await Model.find();
+    const boardstate = await Model.find().sort('index');
     res.json(boardstate);
   })
 
 app.get('/boardstate/:row/:column', async (req, res) => {
     const singleBoardState = await Model.find({row: req.params.row, column: req.params.column});
     res.json(singleBoardState);
+})
+
+app.get('/update', async (req, res) => {
+    // START WORK HERE, WE NEED TO UPDATE MONGODB WITH numb (try only updating records that changed?)
+    const boardstate = await Model.find().sort('index');
+    let numb = updateBoardState(boardstate);
+    res.json(numb);
 })
   
 app.listen(PORT, (error) =>{
